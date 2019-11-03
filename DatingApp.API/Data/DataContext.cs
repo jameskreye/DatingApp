99760a4cede29ee1aba2ed8592002c651ec4a5cb
JamesKreye <jamesylover@gmail.com>
2019-11-03 17:56:23 -0300

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    //la clase DataContext hereda a la clase DbContext del frameWork Entity
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        //informar a Entities sobre nuestras entidades 
+        public DbSet<Value> Values { get; set; }
+    }
+}
